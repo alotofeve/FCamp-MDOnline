3339252304dd1cd2cbb5adf21ab2a680586703f7
ChronossX <michael950513@gmail.com>
2023-06-06 11:00:42 -0400

    BackendCreated

---
+++ MDOlineBackend/src/main/java/com/laioffer/mdolinebackend/MdOlineBackendApplication.java
@@ -0,0 +1,13 @@
+package com.laioffer.mdolinebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdOlineBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdOlineBackendApplication.class, args);
+	}
+
+}
---
+++ MDOlineBackend/src/test/java/com/laioffer/mdolinebackend/MdOlineBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laioffer.mdolinebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdOlineBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
