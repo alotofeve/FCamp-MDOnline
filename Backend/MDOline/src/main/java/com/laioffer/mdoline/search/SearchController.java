e105aff064317035cd9e34c11d25f0dee6b46f37
Juezhi1 <135678122+Juezhi1@users.noreply.github.com>
2023-06-20 23:26:22 -0400

    search

--- Backend/MDOline/src/main/java/com/laioffer/mdoline/search/SearchController.java
+++ Backend/MDOline/src/main/java/com/laioffer/mdoline/search/SearchController.java
@@ -9,5 +9,24 @@ public class SearchController {
         this.searchService = searchService;
     }
 
+    GetMapping("/searchDoctorByName")
+    DoctorEntity searchDoctorByName(String firstname,String lastname){
+        return searchService.searchDoctorByName(String firstname,String lastname);
+    }
+
+    @GetMapping ("/searchDoctorBySpec")
+    List<DoctorEntity> searchDoctorBySpec(String spec){
+        return searchService.searchDoctorBySpec(String spec);
+    }
+    @GetMapping ("/searchDoctorByAll")
+    List<DoctorEntity> searchDoctorByAll(){
+        return searchService.searchDoctorByAll();
+    }
+
+
+
+
+
+
 
 }
