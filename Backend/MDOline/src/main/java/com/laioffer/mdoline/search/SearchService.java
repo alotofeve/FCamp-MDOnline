f9f3dedef8c6be7ed6062181aaf6264983aa50db
Juezhi1 <135678122+Juezhi1@users.noreply.github.com>
2023-06-21 11:32:05 -0400

    search

--- Backend/MDOline/src/main/java/com/laioffer/mdoline/search/SearchService.java
+++ Backend/MDOline/src/main/java/com/laioffer/mdoline/search/SearchService.java
@@ -2,3 +2,20 @@
 
 
 @Service
+
+public abstract class SearchService {
+    DoctorService doctorService;
+
+    public searchService(DoctorService doctorService) {
+        this.doctorService = doctorService;
+    }
+    DoctorEntity searchDoctorByName(String firstname,String lastname) {
+        doctorService.voidfunction(name)
+    }
+    List<DoctorEntity> searchDoctorBySpec(String spec) {
+        doctorService.voidfunction(name)
+    }
+    List<DoctorEntity> searchDoctorByAll() {
+        doctorService.voidfunction(name)
+    }
+}
