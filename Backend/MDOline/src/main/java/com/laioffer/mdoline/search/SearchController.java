a9adbda50581c603629bf68e72e167fc4175af0f
Juezhi1 <135678122+Juezhi1@users.noreply.github.com>
2023-06-20 23:06:11 -0400

    search

--- Backend/MDOline/src/main/java/com/laioffer/mdoline/search/SearchController.java
+++ Backend/MDOline/src/main/java/com/laioffer/mdoline/search/SearchController.java
@@ -5,5 +5,9 @@
 
 public class SearchController {
     private final UserService<DoctorEntity, RegisterDoctorBody> doctorService;
+    SearchController(SearchService searchService) {
+        this.searchService = searchService;
+    }
+
 
 }
